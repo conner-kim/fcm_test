625b872db0b3119aeaaf1ce80baca8261fd142c7
conner-kim <dudrjs145@naver.com>
2021-03-11 17:32:02 +0900

    ServiceWorker 등록

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var isServiceWorkerSupported = 'serviceWorker' in navigator;
 if (isServiceWorkerSupported) {
     navigator.serviceWorker
-        .register('main.js', {scope: '/'})
+        .register('main.js', {scope: '/fcm_test'})
         .then(function (registration) {
             console.log('serviceWorker 등록 성공: ', registration)
         })
         .catch((error) => {
-            console.log('serviceWorker 등록 실패: ', err)
+            console.log('serviceWorker 등록 실패: ', error)
         })
 }
